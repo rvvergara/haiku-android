a1bff3a431a39674a6a1625a19ef11d8e426469c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-14 11:17:48 +0800

    Use drawer in App navigation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import BookingsScreen from './src/screens/BookingsScreen';
 import FeedbackScreen from './src/screens/FeedbackScreen';
 import LoginScreen from './src/screens/LoginScreen';
@@ -11,26 +12,31 @@ import VerifyMessageScreen from './src/screens/VerifyMessageScreen';
 import ScheduleScreen from './src/screens/ScheduleScreen';
 import ClinicsScreen from './src/screens/ClinicsScreen';
 
-const switchNavigator = createSwitchNavigator({
-  authFlow: createStackNavigator({
-    Login: LoginScreen,
-    Signup: SignupScreen,
-    VerifyMessage: VerifyMessageScreen,
-  }),
-  patientFlow: createStackNavigator({
-    Home: HomeScreen,
-    Profile: ProfileScreen,
-    Practitioners: PractitionersScreen,
-    Bookings: BookingsScreen,
-    Feedback: FeedbackScreen,
-  }),
-  practitionerFlow: createStackNavigator({
-    Home: HomeScreen,
-    Profile: ProfileScreen,
-    Schedule: ScheduleScreen,
-    Clinics: ClinicsScreen,
-    Feedback: FeedbackScreen,
-  }),
-});
+const switchNavigator = createSwitchNavigator(
+  {
+    authFlow: createStackNavigator({
+      Login: LoginScreen,
+      Signup: SignupScreen,
+      VerifyMessage: VerifyMessageScreen,
+    }),
+    patientFlow: createDrawerNavigator({
+      Home: HomeScreen,
+      Profile: ProfileScreen,
+      Practitioners: PractitionersScreen,
+      Bookings: BookingsScreen,
+      Feedback: FeedbackScreen,
+    }),
+    practitionerFlow: createDrawerNavigator({
+      Home: HomeScreen,
+      Profile: ProfileScreen,
+      Schedule: ScheduleScreen,
+      Clinics: ClinicsScreen,
+      Feedback: FeedbackScreen,
+    }),
+  },
+  {
+    initialRouteName: 'patientFlow',
+  },
+);
 
 export default createAppContainer(switchNavigator);
