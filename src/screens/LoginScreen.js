7ed5a5ca00abf291b76ef57208b18e5350b02a2c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-14 10:37:04 +0800

    Create authFlow screens

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({});
+
+const LoginScreen = () => (
+  <View>
+    <Text>Login Screen</Text>
+  </View>
+);
+
+export default LoginScreen;
---
+++ src/screens/VerifyMessageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({});
+
+const VerifyMessageScreen = () => (
+  <View>
+    <Text>Verify Message Screen</Text>
+  </View>
+);
+
+export default VerifyMessageScreen;
