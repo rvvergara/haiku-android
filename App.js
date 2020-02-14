2d7584ea672befb5ddc628a5cbfc00750e2806c6
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-14 18:48:05 +0800

    Abstract PatientHomeStack

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
-import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import {createStackNavigator} from 'react-navigation-stack';
-import BookingSelectionScreen from './src/screens/BookingSelectionScreen';
 import BookingsScreen from './src/screens/BookingsScreen';
-import BookingSubmissionScreen from './src/screens/BookingSubmissionScreen';
 import ClinicsScreen from './src/screens/ClinicsScreen';
 import FeedbackScreen from './src/screens/FeedbackScreen';
 import HomeScreen from './src/screens/Home';
 import LoginScreen from './src/screens/LoginScreen';
 import PractitionersScreen from './src/screens/PractitionersScreen';
 import ProfileEditScreen from './src/screens/ProfileEditScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
 import ScheduleScreen from './src/screens/ScheduleScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import VerifyMessageScreen from './src/screens/VerifyMessageScreen';
 import VideoScreen from './src/screens/VideoScreen';
 import ScheduleSubmissionScreen from './src/screens/ScheduleSubmissionScreen';
-import Header from './src/components/Common/Header';
+import PatientHomeStack from './src/navigators/stacks/PatientHomeStack';
 
 const switchNavigator = createSwitchNavigator(
   {
@@ -27,17 +23,7 @@ const switchNavigator = createSwitchNavigator(
       VerifyMessage: VerifyMessageScreen,
     }),
     patientFlow: createDrawerNavigator({
-      Home: createStackNavigator({
-        Home: HomeScreen,
-        PractitionerProfile: ProfileScreen,
-        BookingSelection: BookingSelectionScreen,
-        BookingSubmission: BookingSubmissionScreen,
-      },
-      {
-        navigationOptions: {
-          headerTitle: () => <Header />,
-        },
-      }),
+      Home: PatientHomeStack,
       ProfileEdit: ProfileEditScreen,
       Practitioners: PractitionersScreen,
       Bookings: createStackNavigator({
---
+++ src/navigators/navigationHelpers.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Header from '../components/Common/Header.js';
+
+export const navigationOptions = {
+  headerTitle: () => <Header />,
+};
