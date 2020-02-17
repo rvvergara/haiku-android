4a2dd37a62594555af0307b43371405eb6ccfa9e
Zeha Irawan <jangkaringbumi@gmail.com>
2020-02-17 13:24:30 +0700

    set up combineReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,14 @@
 import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 
 const middleware = [thunk];
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let configureStore = createStore(() => {},
-composeEnhancers(applyMiddleware(...middleware)));
+let configureStore = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware)),
+);
 
 export default configureStore;
