ee614c6d17c1ae668b915132dbab34ed23c4c202
Zeha Irawan <jangkaringbumi@gmail.com>
2020-02-17 14:58:41 +0700

    add checking if token expire

--- src/utils/resolvers.js
+++ src/utils/resolvers.js
@@ -1,9 +1,17 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import { navigate } from './navigationRef';
+import decode from 'jwt-decode';
+import moment from 'moment';
+import {navigate} from './navigationRef';
+
+export const checkIfTokenExp = decoded => {
+  const expirationTime = moment.unix(decoded.exp);
+  const nowTime = moment();
+  return expirationTime < nowTime;
+};
 
 export const resolveToken = async () => {
   const token = await AsyncStorage.getItem('token');
-  if (token) {
+  if (token && checkIfTokenExp(decode(token))) {
     navigate('Home');
   } else {
     navigate('Login');
