62c4f2d151c059b8544ccd1d4271ea0a9d9ca725
Zeha Irawan <jangkaringbumi@gmail.com>
2020-02-17 10:12:33 +0700

    add LogoutButton component

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Button } from 'react-native-elements';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {createDrawerNavigator, DrawerItems} from 'react-navigation-drawer';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import {createStackNavigator} from 'react-navigation-stack';
+import LogoutButton from './src/components/Common/LogoutButton';
 import BookingsStack from './src/navigators/stacks/common/BookingsStack';
 import ClinicsStack from './src/navigators/stacks/common/ClinicsStack';
 import FeedbackStack from './src/navigators/stacks/common/FeedbackStack';
 import HomeStack from './src/navigators/stacks/common/HomeStack';
 import ProfileEditStack from './src/navigators/stacks/common/ProfileEditStack';
 import PractitionersListStack from './src/navigators/stacks/Patient/PractitionersListStack';
+import ScheduleStack from './src/navigators/stacks/Practitioner/ScheduleStack';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import VerifyMessageScreen from './src/screens/VerifyMessageScreen';
-import ScheduleStack from './src/navigators/stacks/Practitioner/ScheduleStack';
 
 const switchNavigator = createSwitchNavigator(
   {
@@ -34,15 +32,7 @@ const switchNavigator = createSwitchNavigator(
       {
         drawerPosition: 'right',
         unmountInactiveRoutes: true,
-        contentComponent: (props) => (
-          <View>
-            <DrawerItems {...props} />
-            <Button
-              title="Logout"
-              onPress={() => console.log('Logging out')}
-            />
-          </View>
-        ),
+        contentComponent: LogoutButton,
       },
     ),
     practitionerFlow: createDrawerNavigator(
@@ -57,6 +47,7 @@ const switchNavigator = createSwitchNavigator(
       {
         drawerPosition: 'right',
         unmountInactiveRoutes: true,
+        contentComponent: LogoutButton,
       },
     ),
   },
