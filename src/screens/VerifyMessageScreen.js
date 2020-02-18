9485afafc4b24754105b576144db7569e20918b0
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-18 11:09:36 +0800

    Add link to Login in VerifyScreen

--- src/screens/VerifyMessageScreen.js
+++ src/screens/VerifyMessageScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
 
 const styles = StyleSheet.create({});
 
-const VerifyMessageScreen = () => (
+const VerifyMessageScreen = ({ navigation }) => (
   <View>
     <Text>Verify Message Screen</Text>
+    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+      <Text>Already verified? Log in now</Text>
+    </TouchableOpacity>
   </View>
 );
 
