b93cd2a780d587c6070234fabd9a9e6062a2c3c9
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-18 12:43:18 +0800

    Create createPatient thunk

---
+++ src/store/thunks/patient.js
@@ -0,0 +1,18 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import { sendAuthorizedRequest } from '../../utils/api';
+import { setCurrentUser } from '../actions/user';
+import { setError } from '../actions/error';
+
+export const createPatient = (params) => async (dispatch, getState) => {
+  const path = 'v1/patients';
+  const token = await AsyncStorage.getItem('token');
+  try {
+    const res = await sendAuthorizedRequest('post', path, token, params);
+    const patient = res.data;
+    const { currentUser } = getState();
+    const updatedUserData = { ...currentUser.data, patient };
+    dispatch(setCurrentUser({ ...currentUser, data: updatedUserData }));
+  } catch (err) {
+    dispatch(setError(err.response.data));
+  }
+};
