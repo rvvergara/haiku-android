f746dd52bace63adf43f2d862e865ad2e5ed47cc
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-19 10:23:29 +0800

    Create ResolveRole screen

---
+++ src/screens/ResolveRole.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { resolveRole } from '../store/thunks/resolvers';
+
+const ResolveRole = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.currentUser.data);
+  useEffect(() => dispatch(resolveRole(userData)), []);
+
+  return null;
+};
+
+export default ResolveRole;
