a149cee4dc4e08778664f2356d6f6cd69b96e1e5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-19 14:35:28 +0800

    Style SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Image } from 'react-native-elements';
 import SignupForm from '../components/Common/SignupForm';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    padding: 50,
+    alignSelf: 'center',
+    marginTop: 70,
+    backgroundColor: 'white',
+    overflow: 'hidden',
+  },
+  logoContainer: {
+    alignSelf: 'center',
+    marginRight: 50,
+    marginBottom: 10,
+    marginTop: 20,
+    position: 'relative',
+  },
+  logo: {
+    width: 210,
+    height: 80,
+  },
+});
 
 const SignupScreen = () => (
-  <View>
-    <Text>Signup Screen</Text>
+  <View style={styles.container}>
+    <View style={styles.logoContainer}>
+      <Image
+        source={{uri: 'https://tinyimg.io/i/uS2trrA.png'}}
+        style={styles.logo}
+      />
+    </View>
     <SignupForm />
   </View>
 );
 
-export default SignupScreen;
+export default {
+  screen: SignupScreen,
+  navigationOptions: {
+    cardStyle: {
+      backgroundColor: '#5073F1',
+    },
+  },
+};
