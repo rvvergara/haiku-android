e82bdca4b89801fe70929d762a6734ce4a0e1c9f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-19 13:24:55 +0800

    Position LoginScreen centered

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { Image } from 'react-native-elements';
 import LoginForm from '../components/Common/LoginForm';
 
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     flex: 1,
+    padding: 50,
     marginBottom: 80,
+    alignSelf: 'center',
+    marginTop: 50,
+    borderColor: 'red',
+    borderWidth: 4,
+  },
+  logoContainer: {
+    alignSelf: 'center',
+    marginRight: 50,
+    marginBottom: 30,
+  },
+  logo: {
+    width: 210,
+    height: 80,
   },
 });
 
 const LoginScreen = () => (
   <View style={styles.container}>
+    <View style={styles.logoContainer}>
+      <Image
+        source={{uri: 'https://tinyimg.io/i/uS2trrA.png'}}
+        style={styles.logo}
+      />
+    </View>
     <LoginForm />
   </View>
 );
