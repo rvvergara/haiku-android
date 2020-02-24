ab497963085b262a2dbf425a80abdbfaf928d9b0
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-24 17:10:44 +0800

    Wrap PractitionersScreen in Spacer

--- src/screens/PractitionersScreen.js
+++ src/screens/PractitionersScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
 import PractitionersList from '../components/Practitioner/PractitionersList';
+import Spacer from '../components/Common/Spacer';
 
 const PractitionersScreen = () => (
-  <View>
+  <Spacer>
     <Text>Practitioner Screen</Text>
     <PractitionersList />
-  </View>
+  </Spacer>
 );
 
 export default PractitionersScreen;
