5bce9d8a2434f6afc6b2834a6f832b03bfa488a9
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-24 17:21:18 +0800

    Remove NavigationEvents from ProfileScreen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {View} from 'react-native';
-import { NavigationEvents } from 'react-navigation';
-import { setPractitioner } from '../store/actions/practitioner';
 import PractitionerProfile from '../components/Practitioner/Profile/Profile';
 import Spacer from '../components/Common/Spacer';
 
 const ProfileScreen = () => {
   const practitioner = useSelector((state) => state.displayedPractitioner);
-  const dispatch = useDispatch();
   return (
     <Spacer>
-      <NavigationEvents
-        onWillBlur={() => dispatch(setPractitioner(null))}
-      />
       <View>
         { practitioner && <PractitionerProfile /> }
       </View>
