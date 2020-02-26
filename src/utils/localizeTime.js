cb7c180ef5bed804f0e0347aaae330c419500b78
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-26 09:26:43 +0800

    Add '0' at start of each hour in localizeTime

--- src/utils/localizeTime.js
+++ src/utils/localizeTime.js
@@ -3,8 +3,8 @@ import moment from 'moment';
 export const localizeBookingSlot = (booking_slot, offset) => {
   const { date, startTime, endTime } = booking_slot;
   const localizedDate = moment(date).format('MMMM DD, YYYY');
-  const localizedStartTime = moment.utc(startTime).add(offset, 'hours').format('LT');
-  const localizedEndTime = moment.utc(endTime).add(offset, 'hours').format('LT');
+  const localizedStartTime = moment.utc(startTime).add(offset, 'hours').format('hh:mm A');
+  const localizedEndTime = moment.utc(endTime).add(offset, 'hours').format('hh:mm A');
   return {
     ...booking_slot,
     date: localizedDate,
