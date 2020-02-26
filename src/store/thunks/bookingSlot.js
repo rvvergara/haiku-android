0207b3a414eac42ac6a339b324837fcd9b4c2463
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-26 15:25:25 +0800

    Create fetchPendingAppointments thunk

--- src/store/thunks/bookingSlot.js
+++ src/store/thunks/bookingSlot.js
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import { sendAuthorizedRequest } from '../../utils/api';
 import { listSlots } from '../actions/openSlot';
+import { listPendingAppointments } from '../actions/pendingAppointments';
 import { setErrors } from '../actions/error';
 import { localizeBookingSlot } from '../../utils/localizeTime';
 import { navigate } from '../../utils/navigationRef';
@@ -32,5 +33,25 @@ export const bookSlot = (params, slotId) => async (dispatch) => {
     navigate('Profile');
   } catch (err) {
     dispatch(setErrors([err.response.data.error]));
+  }
+};
+
+export const fetchPendingAppointments = (profile, role) => async (dispatch) => {
+  const path = role === 'PATIENT'
+    ? `v1/patients/${profile.id}/booking-slots?status=PENDING&include=practitioner`
+    : `v1/practitioners/${profile.id}/booking-slots?status=PENDING&include=patient`;
+
+  const token = await AsyncStorage.getItem('token');
+
+  try {
+    const res = await sendAuthorizedRequest('get', path, token);
+    const { booking_slots } = res.data;
+    let pendingAppointments = booking_slots;
+    if (role === 'PRACTITIONER') {
+      pendingAppointments = pendingAppointments.filter((slot) => slot.patient !== null);
+    }
+    return dispatch(listPendingAppointments(pendingAppointments));
+  } catch (err) {
+    return err;
   }
 };
