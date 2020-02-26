632feb5146da6de702b29517ffa45e2b438cd04c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-26 16:16:28 +0800

    Create PendingList component

--- src/components/Common/Appointments/PendingList.js
+++ src/components/Common/Appointments/PendingList.js
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { View, StyleSheet } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text, ListItem } from 'react-native-elements';
+import moment from 'moment';
 import { fetchPendingAppointments } from '../../../store/thunks/bookingSlot';
 import { listPendingAppointments } from '../../../store/actions/pendingAppointments';
+
+const styles = StyleSheet.create({
+  listContainer: {
+    margin: 10,
+    borderRadius: 10,
+  },
+});
 
 const PendingList = () => {
   const currentUserData = useSelector((state) => state.currentUser.data);
 
   const { role } = currentUserData;
 
+  const appointee = role === 'PATIENT' ? 'practitioner' : 'patient';
+
   const profile = currentUserData.patient || currentUserData.practitioner;
 
   const dispatch = useDispatch();
+
+  const pendingAppointments = useSelector((state) => state.pendingAppointments);
 
   useEffect(() => {
     dispatch(fetchPendingAppointments(profile, role));
@@ -22,7 +34,19 @@ const PendingList = () => {
 
   return (
     <View>
-      <Text>Pending List Here</Text>
+      {
+        pendingAppointments.map((appointment) => (
+          <ListItem
+            key={appointment.id}
+            containerStyle={styles.listContainer}
+            leftAvatar={{source: { uri: appointment[appointee].image }}}
+            title={`${appointment[appointee].firstName} ${appointment[appointee].lastName}`}
+            subtitle={moment(appointment.date).format('MMMM DD, YYYY')}
+            bottomDivider
+            chevron
+          />
+        ))
+      }
     </View>
   );
 };
