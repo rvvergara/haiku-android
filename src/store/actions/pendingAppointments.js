c5bdd5982bf8055237a38d94c3d8b8526830b083
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-26 14:19:12 +0800

    Remove adding pending appointment

--- src/store/actions/pendingAppointments.js
+++ src/store/actions/pendingAppointments.js
@@ -1,11 +1,6 @@
-import { LIST_PENDING_APPOINTMENTS, ADD_PENDING_APPOINTMENT} from './types';
+import { LIST_PENDING_APPOINTMENTS } from './types';
 
 export const listPendingAppointments = (appointments) => ({
   type: LIST_PENDING_APPOINTMENTS,
   appointments,
-});
-
-export const addPendingAppointment = (appointment) => ({
-  type: ADD_PENDING_APPOINTMENT,
-  appointment,
 });
--- src/store/reducers/pendingAppointments.js
+++ src/store/reducers/pendingAppointments.js
@@ -1,11 +1,9 @@
-import { LIST_PENDING_APPOINTMENTS, ADD_PENDING_APPOINTMENT } from '../actions/types';
+import { LIST_PENDING_APPOINTMENTS } from '../actions/types';
 
 export default (state = [], action) => {
   switch (action.type) {
     case LIST_PENDING_APPOINTMENTS:
       return action.appointments;
-    case ADD_PENDING_APPOINTMENT:
-      return [...state, action.appointment];
     default:
       return state;
   }
