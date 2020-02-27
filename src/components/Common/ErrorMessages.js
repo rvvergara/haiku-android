91636513f91c1e1560785aa5b0757cbb3ae604b7
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-27 11:39:59 +0800

    Refactor ErrorMessages

--- src/components/Common/ErrorMessages.js
+++ src/components/Common/ErrorMessages.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FlatList, StyleSheet } from 'react-native';
-import { Text } from 'react-native-elements';
+import { StyleSheet } from 'react-native';
+import { Text, ListItem } from 'react-native-elements';
 
 const styles = StyleSheet.create({
+  listContainer: {
+    backgroundColor: 'red',
+    alignSelf: 'center',
+    padding: 5,
+    width: '80%',
+    marginBottom: 5,
+  },
   error: {
-    color: 'red',
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 
 });
 
-const ErrorMessages = ({ errors }) => (
-  <FlatList
-    data={errors}
-    keyExtractor={(err) => err}
-    renderItem={({item}) => <Text style={styles.error}>{item}</Text>}
+const ErrorMessages = ({ errors }) => (errors.map((err) => (
+  <ListItem
+    key={err}
+    containerStyle={styles.listContainer}
+    title={<Text style={styles.error}>{err}</Text>}
   />
-);
+)));
 
 ErrorMessages.propTypes = {
   errors: PropTypes.instanceOf(Object).isRequired,
