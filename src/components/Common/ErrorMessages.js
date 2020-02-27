f26e3a0c01dce92c2a99aea1c94f35300238340f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-27 11:42:00 +0800

    Abstract errorStyles

--- src/components/Common/ErrorMessages.js
+++ src/components/Common/ErrorMessages.js
@@ -2,23 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet } from 'react-native';
 import { Text, ListItem } from 'react-native-elements';
+import { errorStyles } from '../../style-objects/errorStyles';
 
-const styles = StyleSheet.create({
-  listContainer: {
-    backgroundColor: 'red',
-    alignSelf: 'center',
-    padding: 5,
-    width: '80%',
-    marginBottom: 5,
-  },
-  error: {
-    color: 'white',
-    fontSize: 15,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-  },
-
-});
+const styles = StyleSheet.create(errorStyles);
 
 const ErrorMessages = ({ errors }) => (errors.map((err) => (
   <ListItem
---
+++ src/style-objects/errorStyles.js
@@ -0,0 +1,15 @@
+export const errorStyles = {
+  listContainer: {
+    backgroundColor: 'red',
+    alignSelf: 'center',
+    padding: 5,
+    width: '80%',
+    marginBottom: 5,
+  },
+  error: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+};
