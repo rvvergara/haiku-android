f4dbbcb7f33a63bf7cd57da19f6e4d1996a8a7b6
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-28 11:51:39 +0800

    Add Logout icon

--- src/components/Common/CustomDrawer.js
+++ src/components/Common/CustomDrawer.js
@@ -4,6 +4,7 @@ import {
 } from 'react-native';
 import {DrawerItems} from 'react-navigation-drawer';
 import {useDispatch} from 'react-redux';
+import Icon from 'react-native-vector-icons/AntDesign';
 import {logout} from '../../store/thunks/user';
 
 const styles = StyleSheet.create({
@@ -21,8 +22,14 @@ const styles = StyleSheet.create({
     borderTopWidth: 0.95,
     paddingLeft: 15,
     paddingTop: 20,
-    flexDirection: 'column',
+    flexDirection: 'row',
     flex: 2,
+  },
+  logoutIcon: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+    marginRight: 10,
   },
   itemsContainer: {
     flex: 3,
@@ -43,6 +50,7 @@ const CustomDrawer = (props) => {
       </View>
 
       <TouchableOpacity style={styles.logoutContainer} onPress={handleLogout}>
+        <Icon name="logout" style={styles.logoutIcon} />
         <Text style={styles.logoutText}>Logout</Text>
       </TouchableOpacity>
     </View>
