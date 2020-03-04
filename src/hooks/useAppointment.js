d282491e3e50e8d53e4d25d9058107c284bf2d0b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-03-04 14:04:43 +0800

    Convert useAppointment to be flexible

--- src/hooks/useAppointment.js
+++ src/hooks/useAppointment.js
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPendingAppointments } from '../store/thunks/bookingSlot';
+import { fetchPendingAppointments, fetchAllConfirmedAppointments } from '../store/thunks/bookingSlot';
 import { listPendingAppointments } from '../store/actions/pendingAppointments';
 
-export default () => {
+export default (appointmentType) => {
   const currentUserData = useSelector((state) => state.currentUser.data);
 
   const { role } = currentUserData;
@@ -14,20 +14,25 @@ export default () => {
 
   const dispatch = useDispatch();
 
-  const pendingAppointments = useSelector((state) => state.pendingAppointments);
+  const shownAppointments = appointmentType === 'PENDING' ? useSelector((state) => state.pendingAppointments)
+    : useSelector((state) => state.appointments);
 
   const prefix = appointee === 'practitioner' ? 'Dr.' : '';
 
   const fullName = (firstName, lastName) => `${prefix} ${firstName} ${lastName}`;
 
   useEffect(() => {
-    dispatch(fetchPendingAppointments(profile, role));
+    if (appointmentType === 'PENDING') {
+      dispatch(fetchPendingAppointments(profile, role));
+    } else {
+      dispatch(fetchAllConfirmedAppointments(profile.id, role));
+    }
   }, []);
 
   useEffect(() => () => dispatch(listPendingAppointments([])), []);
 
   return {
-    pendingAppointments,
+    shownAppointments,
     fullName,
     appointee,
     currentUserData,
